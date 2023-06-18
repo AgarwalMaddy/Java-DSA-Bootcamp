9c1b3df64adc1a999ee002e5bc3a7c9fd7fa35ac
Madhur Agarwal <85205360+AgarwalMaddy@users.noreply.github.com>
2023-06-18 18:46:31 +0530

    Create 04. Richest Customer Wealth.java

---
+++ 12. Arrays and Array Lists/Section 1 . Easy/04. Richest Customer Wealth.java
@@ -0,0 +1,18 @@
+// 1672. Richest Customer Wealth
+// https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int result = Integer.MIN_VALUE;
+        for(int[] arr : accounts){
+            int sum = 0;
+            for(int i : arr){
+                sum += i;
+            }
+            if(sum > result){
+                result = sum;
+            }
+        }
+        return result;
+    }
+}
