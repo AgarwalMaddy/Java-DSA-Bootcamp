52a0a8c99621026c767238dac535e92eb0ca9c3b
Madhur Agarwal <85205360+AgarwalMaddy@users.noreply.github.com>
2023-06-18 18:55:30 +0530

    Create 05. Shuffle The Array.java

---
+++ 12. Arrays and Array Lists/Section 1 . Easy/05. Shuffle The Array.java
@@ -0,0 +1,15 @@
+// 1470. Shuffle the Array
+// https://leetcode.com/problems/shuffle-the-array/description/
+
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[n * 2];
+        for(int i = 0 ; i < n  ; i++){
+            result[i * 2] = nums[i];
+            result[i * 2 + 1] = nums[i + n];
+        }
+
+        return result;
+    }
+}
