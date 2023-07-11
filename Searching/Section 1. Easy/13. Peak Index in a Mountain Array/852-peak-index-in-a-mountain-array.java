232416d94f6505b15a936016f12f07141dd54f67
AgarwalMaddy <madhur1812agarwal@gmail.com>
2023-07-11 22:02:21 +0530

    Peak Index

--- Searching/Section 1. Easy/13. Peak Index in a Mountain Array/852-peak-index-in-a-mountain-array.java
+++ Searching/Section 1. Easy/13. Peak Index in a Mountain Array/852-peak-index-in-a-mountain-array.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
-        if(arr.length == 1 ){
-            return 0;
-        }
+    public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end = arr.length-1;
+        int end = arr.length - 1;
         while(start < end){
             int mid = start + (end - start)/2;
-            if(arr[mid] > arr[mid+1]){
+            if(arr[mid] > arr[mid + 1]){
                 end = mid;
-            } else {
+            }
+            if(arr[mid] < arr[mid + 1]){
                 start = mid + 1;
             }
         }
