3fc5c83eb38a3c72cf47214f7a9311a9f0d344c6
Madhur Agarwal <85205360+AgarwalMaddy@users.noreply.github.com>
2023-07-21 09:10:47 +0530

    Time: 1 ms (100.00%), Space: 39.6 MB (76.31%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,20 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0){
-            return 0;
-        }
-        int start = 1;
-        int end = x;
+        if(x == 0 || x == 1){
+            return x;
+        } 
+        int start = 2;
+        int end = x / 2;
         while(start <= end){
             int mid = start + (end - start)/2;
-            if(mid == x/mid){
+            if((long)mid * mid == x){
                 return mid;
-            } else if(mid > x/mid){
+            } else if((long)mid * mid > x){
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
+        
         return end;
     }
 }
